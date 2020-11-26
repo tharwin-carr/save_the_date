defda451a1436a524ee207d5637bb2ce13047d2a
Tharwin <tharwin.carr08@gmail.com>
2020-11-26 14:13:44 -0800

    trying to fix cors

--- src/components/DatesPage.js
+++ src/components/DatesPage.js
@@ -19,7 +19,7 @@ export default class DatesPage extends Component {
         fetch(`${config.API_ENDPOINT}/dates`, {
             method: 'GET',
             headers: {
-                'content-type': 'text/plain'
+                'content-type': 'application/json'
             }
         })
         .then(res => res.json())
