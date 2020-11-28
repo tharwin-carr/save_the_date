59cffbd1995fc6deedf4e7945bd6162939fda05b
Tharwin <tharwin.carr08@gmail.com>
2020-11-28 15:54:04 -0800

    fixed typo

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ export default class App extends Component {
         if(!datesRes.ok)
         return datesRes.json().then(e => Promise.reject(e))
 
-        return Promise.all([datesRes.json()])
+        return [datesRes.json()]
       })
       .then(([dates]) => {
         this.setState({dates})
